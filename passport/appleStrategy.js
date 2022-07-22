bbc05d3215314d0d6851270ff1ad7a9fb88679ca
권태현 <kygkth2011@gmail.com>
2022-07-23 01:31:51 +0900

    resolve nico passport err

--- passport/appleStrategy.js
+++ passport/appleStrategy.js
@@ -1,33 +1,39 @@
 const passport = require('passport');
 const AppleStrategy = require('@nicokaiser/passport-apple').Strategy;
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = () => {
-    passport.use(new AppleStrategy(
-      {
-        clientID: process.env.APPLE_CLIENT_ID,
-        teamID: process.env.APPLE_TEAM_ID,
-        keyID: process.env.APPLE_KEY_ID,
-        key : fs.readFileSync(path.join(__dirname, 'AuthKey_689F483NJ3.p8')),
-        callbackURL: "https://chocobread.shop/auth/apple/callback",
-        scope: ['name', 'email']
-      },
-      (accessToken, refreshToken, profile, done) => {
-          const {
-              id,
-              name: { firstName, lastName },
-              email
-          } = profile;
+  passport.use(
+    'apple',
+    new AppleStrategy(
+        {
+          clientID: process.env.APPLE_CLIENT_ID,
+          teamID: process.env.APPLE_TEAM_ID,
+          keyID: process.env.APPLE_KEY_ID,
+          key : fs.readFileSync(path.join(__dirname, 'AuthKey_689F483NJ3.p8')),
+          callbackURL: "https://chocobread.shop/auth/apple/callback",
+          scope: ['name', 'email']
+        },
+        (accessToken, refreshToken, profile, done) => {
+            const {
+                id,
+                name: { firstName, lastName },
+                email
+            } = profile;
 
-          // Create or update the local user here.
-          // Note: name and email are only submitted on the first login!
+            // Create or update the local user here.
+            // Note: name and email are only submitted on the first login!
 
-          done(null, {
-              id,
-              email,
-              name: { firstName, lastName }
-          });
-      }
-  );
-)};
-  
+            done(null, {
+                id,
+                email,
+                name: { firstName, lastName }
+            });
+        }
+    )
+);
+}
+
