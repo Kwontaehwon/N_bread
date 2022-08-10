0210286573c6fc6eab8604626171013f9642be6c
EC2 Default User <ec2-user@ip-172-31-36-54.ap-northeast-2.compute.internal>
2022-08-10 05:45:16 +0000

    add log to controllers/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -233,6 +233,7 @@ const putUserNick = async (req, res, next) => {
 const checkUserNick = async (req, res, next) => {
   try {
     const { nick } = req.params.nick;
+    console.log("nickname from params is "+nick);
     const user = await User.findOne({ where: { Id: req.params.userId } });
     if (!user) {
       logger.info(`userId : ${req.params.userId}에 해당되는 유저가 없습니다.`);
