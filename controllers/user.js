758aead99427ed35e7755034653604bd8b89eee9
kite707 <kite707707@gmail.com>
2022-08-11 20:46:09 +0900

    find deleted user at [GET] users/:userId

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,7 @@ function jsonResponse(res, code, message, isSuccess, result){
 // GET users/:userId
 const getUser = async (req, res, next) => {
     try{
-        const user = await User.findOne({where : { Id : req.params.userId}});
+        const user = await User.findOne({where : { Id : req.params.userId},paranoid:false});
         if(!user){
             return jsonResponse(res, 404, "userId에 해당되는 유저가 없습니다.", false, null)
         }
@@ -28,6 +28,7 @@ const getUser = async (req, res, next) => {
             nick : user.nick,
             provider : user.provider,
             addr : user.curLocation3,
+            deletedAt:user.deletedAt
         }
         logger.info(`GET users/:userId | userId : ${req.params.userId} 의 유저 정보를 반환합니다.`);
         return jsonResponse(res, 200, "userId의 정보를 반환합니다.", true, result)
