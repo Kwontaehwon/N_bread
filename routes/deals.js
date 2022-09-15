48bbbf7dac57c1f49dabd972b453e88efe2a0fe0
kite707 <kite707707@gmail.com>
2022-09-15 14:41:39 +0900

    CB-238, deals.js : modify Date.now() to new Date(Date.now())

--- routes/deals.js
+++ routes/deals.js
@@ -108,7 +108,7 @@ router.get('/all/:region', async (req, res, next) => {
       if (toSetStatus['dealDate'] < new Date(Date.now())){
         if (toSetStatus['currentMember'] === toSetStatus['totalMember']) toSetStatus['status']="모집완료"
         else toSetStatus['status']="모집실패"
-      } else if(toSetStatus['dealDate']<Date.now()){
+      } else if (toSetStatus['dealDate'] < new Date(Date.now())){
         toSetStatus['status']="거래완료";
       }
       else if(toSetStatus['currentMember']===toSetStatus['totalMember']){
