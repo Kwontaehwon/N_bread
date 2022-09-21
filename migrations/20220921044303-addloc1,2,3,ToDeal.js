3f83ed0fb4dc8c1167bb8d1fb0f79df946c683b1
kite707 <kite707707@gmail.com>
2022-09-21 14:19:34 +0900

    CB-151, modify region to loc1 and add loc2,3 to dealTable by sequelize migration

--- migrations/20220921044303-addloc1,2,3,ToDeal.js
+++ migrations/20220921044303-addloc1,2,3,ToDeal.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('deals', 'loc1', { type: Sequelize.STRING() });
+    await queryInterface.renameColumn('deals', 'region', 'loc1');
     await queryInterface.addColumn('deals', 'loc2', { type: Sequelize.STRING() });
     await queryInterface.addColumn('deals', 'loc3', { type: Sequelize.STRING() });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('deals', 'loc1', { type: Sequelize.STRING() });
-    await queryInterface.removeColumn('deals', 'loc2', { type: Sequelize.STRING() });
-    await queryInterface.removeColumn('deals', 'loc3', { type: Sequelize.STRING() });
+    await queryInterface.renameColumn('deals', 'loc1', 'region');
+    await queryInterface.removeColumn('deals', 'loc2');
+    await queryInterface.removeColumn('deals', 'loc3');
   }
 };
