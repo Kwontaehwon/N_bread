b65788bb5aae512015e8b70203a80f4556fbcba2
kite707 <kite707707@gmail.com>
2022-09-21 14:20:59 +0900

    CB-151, modify deal create api to save loc1,2,3 and clean comments

--- routes/deals.js
+++ routes/deals.js
@@ -190,19 +190,12 @@ router.post('/create', verifyToken, async (req, res, next) => {
       dealPlace : place,
       currentMember : 1, // 내가 얼마나 가져갈지 선택지를 줘야할듯
       userId : user.id,
-      region:region
+      loc1: user.curLocation1,
+      loc2: user.curLocation2,
+      loc3: user.curLocation3,
     })
-    console.log("image link is added");
-    console.log("deal id is " + deal.id);
     await group.update({ dealId : deal.id }); // 업데이트
     logger.info(`userId : ${deal.id} 거래가 생성되었습니다.`);
-    // const dealEnd = new Date(deal.dealDate);
-    // const dealDeadLine = new Date();
-    // dealDeadLine.setDate(dealEnd.getDate() - 3);
-    // schedule.scheduleJob(dealDeadLine, async() => {
-    //   await deal.update({isDealDone : true});
-    // })
-    // logger.info(`dealId ${deal.id} 의 Deal의 모집 마감 시간이 ${dealDeadLine}으로 스케줄 되었습니다.`);
     return jsonResponse(res, 200, "거래가 생성되었습니다", true, deal);
   } catch (error) {
     logger.error(error);
