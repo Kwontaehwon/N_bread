2ef211a2d505c6d213280ff1902c976400745928
kite707 <kite707707@gmail.com>
2022-11-21 03:06:56 +0900

    fix(price): mecab을 파이썬을 이용해 실행

--- routes/price.js
+++ routes/price.js
@@ -36,17 +36,7 @@ router.use(express.json());
 // POST price/:productName
 router.post('/:dealId',async (req, res) => {
     //python test
-    const result_01 =await spawn('python3', ['./routes/getTopic.py',"출력해주세요"],);
-
-    result_01.stdout.on('data', (result) => {
-        console.log('python test')
-        console.log(result.toString());
-    });
-
-    result_01.stderr.on('data', (result) => {
-        console.log('python test')
-        console.log(result.toString());
-    });
+   
 
     try{
         const deal = await Deal.findOne({ where: { id: req.params.dealId }, paranoid: false });
@@ -124,93 +114,84 @@ router.post('/:dealId',async (req, res) => {
     //const text = deal.title;
     logger.info(`[최저가 저장] \"${title}\"에서 상품명 추출을 시도합니다.`);
     const text = title;
+    
     var answer = "";
-    var endOfI = 0;
-    var testword = "진라면 40봉지 20봉지씩 소분하실 분!!"
-    mecab.pos(testword,function(err,result){
-        console.log('mecab result isssss ');
-        console.log(result.length);
-    })
-
-    mecab.pos(text, function (err, result) {
         try {
-            logger.info(`mecab result is ${result}`);
-            logger.info(`mecab result is ${result.length}`);
-            for (i = 0; i < result.length - 1; i++) {
-                if (result[i][1] == 'SL' || result[i][1] == 'NNG' || result[i][1] == 'NNP') {
-                    if (result[i + 1][1] == 'SL' || result[i + 1][1] == 'NNG' || result[i + 1][1] == 'NNP') {
-                        answer += result[i][0];
-                        answer += " ";
-                    } else {
-                        endOfI = i;
-                        answer += result[endOfI][0];
-                        break;
-                    }
+            const result_01 = await spawn('python3', ['./routes/getTopic.py', title],);
+
+            await result_01.stdout.on('data', async (result) => {
+                console.log(result)
+                console.log(result.toString());
+                answer = result.toString();
+                logger.info(`추출된 상품명은 ${answer}입니다.`)
+
+                //상품명 기반 검색 시작
+                try {
+                    const productName = answer + gramToAdd;
+                    //const productName = "";
+                    logger.info(`${productName}로 네이버 쇼핑에 검색을 시도합니다.`);
+                    const client_id = env.NAVER_DEVELOPER_CLIENTID;
+                    const client_secret = env.NAVER_DEVELOPER_CLIENTSECRET;
+                    var url = 'https://openapi.naver.com/v1/search/shop.json?query=' + encodeURI(productName) + "&sort=asc&display=4"; // JSON 결과
+                    var options = {
+                        url: url,
+                        headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
+                    };
+                    request.get(options, async (error, response, body) => {
+                        if (!error && response.statusCode == 200) {
+                            var item = JSON.parse(body)['items'];
+                            const existDeal = await Price.findOne({ where: { dealId: req.params.dealId, mallName: { [Op.not]: 'N빵' } } });
+                            if (!existDeal) {
+                                for (i = 0; i < item.length; i++) {
+                                    await Price.create({
+                                        dealId: req.params.dealId,
+                                        title: item[i]["title"],
+                                        link: item[i]["link"],
+                                        image: item[i]["image"],
+                                        lPrice: (item[i]["lprice"] * 1) + 3000,
+                                        hPrice: item[i]["hprice"],
+                                        mallName: item[i]["mallName"],
+                                        productId: item[i]["productId"],
+                                        productType: item[i]["productType"],
+                                        brand: item[i]["brand"],
+                                        maker: item[i]["maker"],
+                                        category1: item[i]["category1"],
+                                        category2: item[i]["category2"],
+                                        category3: item[i]["category3"],
+                                        category4: item[i]["category4"],
+                                    })
+                                }
+                            }
+
+                            for (i = 0; i < item.length; i++) {
+                                item[i].lprice = item[i].lprice * 1 + 3000;
+                                jsonArray.push(item[i]);
+                            }
+                            if (item.length === 0) {
+                                return jsonResponse(res, 403, `네이버 쇼핑 검색 결과가 없습니다. 검색어는 ${productName}입니다.`, false, null)
+                            }
+                            return jsonResponse(res, 200, "", true, jsonArray);
+                        } else {
+                            return jsonResponse(res, 404, `[Lowest Price] 네이버 쇼핑 api error : 검색어는 ${productName}입니다.`, false, null)
+                        }
+                    });
+                } catch (error) {
+                    logger.error(error);
+                    return jsonResponse(res, 500, "[Lowest Price] price/:productName 서버 에러", false, result)
                 }
-            }
-            if (endOfI = result.length - 2 && (result[result.length - 1][1] == 'SL' || result[result.length - 1][1] == 'NNG' || result[result.length - 1][1] == 'NNP')) {
-                answer += result[result.length - 1][0];
-            }
-            logger.info(`추출된 상품명은 ${answer}입니다.`)
+            });
+
+            await result_01.stderr.on('data', (result) => {
+                console.log('python test')
+                console.log(result.toString());
+            });
+            
         } catch (error) {
             logger.info(`상품명 추출 중 오류가 발생하였습니다.`);
             return jsonResponse(res, 402, `${title}에서 상품명 추출 중 오류가 발생하였습니다.`, false, error)
         }
         
-        try {
-            const productName = answer+gramToAdd;
-            //const productName = "";
-            logger.info(`${productName}로 네이버 쇼핑에 검색을 시도합니다.`);
-            const client_id = env.NAVER_DEVELOPER_CLIENTID;
-            const client_secret = env.NAVER_DEVELOPER_CLIENTSECRET;
-            var url = 'https://openapi.naver.com/v1/search/shop.json?query=' + encodeURI(productName)+"&sort=asc&display=4"; // JSON 결과
-            var options = {
-                url: url,
-                headers: { 'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
-            };
-            request.get(options, async(error, response, body) => {
-                if (!error && response.statusCode == 200) {
-                    var item = JSON.parse(body)['items'];
-                    const existDeal = await Price.findOne({where:{dealId:req.params.dealId,mallName:{[Op.not]:'N빵'}}});
-                    if(!existDeal){
-                        for (i = 0; i < item.length; i++) {
-                            await Price.create({
-                                dealId: req.params.dealId,
-                                title: item[i]["title"],
-                                link: item[i]["link"],
-                                image: item[i]["image"],
-                                lPrice:(item[i]["lprice"]*1)+3000,
-                                hPrice: item[i]["hprice"],
-                                mallName: item[i]["mallName"],
-                                productId: item[i]["productId"],
-                                productType: item[i]["productType"],
-                                brand: item[i]["brand"],
-                                maker: item[i]["maker"],
-                                category1: item[i]["category1"],
-                                category2: item[i]["category2"],
-                                category3: item[i]["category3"],
-                                category4: item[i]["category4"],
-                            })
-                        }
-                    }
-                    
-                    for (i = 0; i < item.length;i++){
-                        item[i].lprice = item[i].lprice*1+3000;
-                        jsonArray.push(item[i]);
-                    }
-                    if(item.length===0){
-                        return jsonResponse(res, 403,`네이버 쇼핑 검색 결과가 없습니다. 검색어는 ${productName}입니다.`,false,null)
-                    }
-                    return jsonResponse(res, 200, "", true, jsonArray);
-                } else {
-                    return jsonResponse(res, 404, `[Lowest Price] 네이버 쇼핑 api error : 검색어는 ${productName}입니다.`, false, null)
-                }
-            });
-        } catch (error) {
-            logger.error(error);
-            return jsonResponse(res, 500, "[Lowest Price] price/:productName 서버 에러", false, result)
-        }
-     });
+        
     
 });
 // GET price/:dealId
