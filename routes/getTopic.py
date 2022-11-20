360a0dd2ea4203fea7053acc34b9742690960ef3
kite707 <kite707707@gmail.com>
2022-11-21 03:30:58 +0900

    fix(price): mecab 서버용으로 변경

--- routes/getTopic.py
+++ routes/getTopic.py
@@ -3,11 +3,11 @@ from konlpy.tag import Mecab
 
 def getName(title):
     #print ("title to extract product name is ",title)
-    # mecab = Mecab()
-    # result = mecab.pos(title)
+    mecab = Mecab()
+    result = mecab.pos(title)
     endOfI = 0
     answer=""
-    result = [('진라면', 'NNP'), ('매운맛', 'NNG'), ('40', 'SN'), ('봉지', 'NNBC'), ('20', 'SN'), ('봉지', 'NNBC'), ('씩', 'XSN'), ('소분', 'NNG'), ('하', 'XSV'), ('실', 'EP+ETM'), ('분', 'NNB'), ('!', 'SF')]
+    #result = [('진라면', 'NNP'), ('매운맛', 'NNG'), ('40', 'SN'), ('봉지', 'NNBC'), ('20', 'SN'), ('봉지', 'NNBC'), ('씩', 'XSN'), ('소분', 'NNG'), ('하', 'XSV'), ('실', 'EP+ETM'), ('분', 'NNB'), ('!', 'SF')]
 
     for i,x in enumerate(result):
         if (x[1] == 'SL')or (x[1] == 'NNG')or (x[1] == 'NNP'):
