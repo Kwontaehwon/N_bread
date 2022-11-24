80e0d6c8d52c202d60be0f960313fae928723790
kite707 <kite707707@gmail.com>
2022-11-24 19:14:06 +0900

    fix(price): 가격 정보 조회 성공 시 return하도록 수정

--- routes/getTopic.py
+++ routes/getTopic.py
@@ -3,29 +3,33 @@ from konlpy.tag import Mecab
 
 def getName(title):
     #print ("title to extract product name is ",title)
-    mecab = Mecab()
-    result = mecab.pos(title)
-    endOfI = 0
-    answer=""
-    if(len(result)>1):
-        for i,x in enumerate(result):
-         if (x[1] == 'SL')or (x[1] == 'NNG')or (x[1] == 'NNP'):
-             if (result[i + 1][1] == 'SL')or (result[i + 1][1] == 'NNG')or (result[i + 1][1] == 'NNP'):
-                 answer+=x[0]
-                 answer+=" "
-             else:
-                endOfI = i
-                answer+=result[endOfI][0]
-                break
 
-        if ((endOfI == len(result)-1)and((result[len(result)-1][1]=='SL')or(result[len(result)-1][1]=='NNG')or(result[len(result)-1][1]=='NNP'))):
-            answer+=result[len(result)-1][0]
+    try:
+        mecab = Mecab()
+        #mecab = Mecab("/opt/homebrew/lib/mecab/dic/mecab-ko-dic")
         
+        result = mecab.pos(title)
+        answer=""
+        frequent = ['같이', '사실','사요','분','구해','kg','소분','g','씩','사서','하나','cm','판매','봉','ml','L']
+        if '몰티져스' in title:
+            answer = '몰티져스'
+        else:
+            for x in result[:]:
+                if ('+' in x[1]) or (x[1] == 'MAG') or (x[1] == 'SF') or ('NNB' in x[1]) or (x[1] == 'SN')or (x[1] == 'SY')or (x[1] == 'XSV')or (x[0]in frequent) or (x[1] == 'SC')or (x[1] == 'JKB'):
+                    result.remove(x)
+            for x in result:
+                answer+=x[0]
+            if '(인당)' in answer:
+                answer=answer.replace('인당','')
+        if(answer==""):
+            answer="오류발생"
+        
+        print(answer,end='')
+    except:
+        print('오류발생',end='')
 
     
-    #print(mecab.pos(title))
-    print(answer,end='')
-    return answer
+
 
 if __name__ == '__main__':
     getName(sys.argv[1])
