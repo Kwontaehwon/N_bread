e2001669ebd37280bef9aaa1af25dbb1dd88a226
kite707 <kite707707@gmail.com>
2023-03-18 22:06:51 +0900

    userService함수 사용 시 service/index에서 userService require해 사용하도록 변경

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -12,64 +12,56 @@ const { any, reject } = require('bluebird');
 const { response } = require('express');
 const { resolve } = require('path');
 const sequelize = require('../database/models');
-const {
-  getUser,
-  getMypageDeals,
-  getNaverGeoLocation,
-  getUserLocation,
-  putUserNick,
-  checkUserNick,
-  postReportUser,
-  isSetNickname,
-  putKakaoUserNick,
-  getLocationByNaverMapsApi,
-  setLocationByNaverMapsApi,
-  deletelocation,
-  addLocation,
-} = require('../controllers/userController');
+const { userService } = require('../service/');
 
 const userRouter = express.Router();
 
 userRouter.use(express.json());
 
 // 마이페이지 거래 내역:수정중
-userRouter.get('/deals/:userId', verifyToken, getMypageDeals);
+userRouter.get('/deals/:userId', verifyToken, userService.getMypageDeals);
 
 // 유저 현재 위치 등록 (naver GeoLocation) -> verifyToken?
-userRouter.post('/location/:userId/:latitude/:longitude', getNaverGeoLocation);
+userRouter.post(
+  '/location/:userId/:latitude/:longitude',
+  userService.getNaverGeoLocation,
+);
 
 // 유저 DB에서 저장된 위치 GET -> verifyToken 삭제?
-userRouter.get('/location', verifyToken, getUserLocation);
+userRouter.get('/location', verifyToken, userService.getUserLocation);
 
 // 유저 정보 GET
-userRouter.get('/:userId', getUser);
+userRouter.get('/:userId', userService.getUser);
 
 // 유저 닉네임 변경
-userRouter.put('/:userId', putUserNick);
+userRouter.put('/:userId', userService.putUserNick);
 
 //유저 닉네임 중복체크
-userRouter.get('/check/:userId/:nick', checkUserNick); // 닉네임 중복체크를 하는데 userId가 필요한 이유는?
+userRouter.get('/check/:userId/:nick', userService.checkUserNick); // 닉네임 중복체크를 하는데 userId가 필요한 이유는?
 
 // 유저 신고
-userRouter.post('/report/:userId', verifyToken, postReportUser);
+userRouter.post('/report/:userId', verifyToken, userService.postReportUser);
 
 //회원가입 완료 여부
-userRouter.get('/check/:userId', isSetNickname);
+userRouter.get('/check/:userId', userService.isSetNickname);
 
 //reverse geocoding을 통해 위치 가져오기
-userRouter.get('/location/:latitude/:longitude', getLocationByNaverMapsApi);
+userRouter.get(
+  '/location/:latitude/:longitude',
+  userService.getLocationByNaverMapsApi,
+);
 
 //reverse geocoding을 통해 가져온 위치 db에 저장
 userRouter.post(
   '/location/:userId/:loc1/:loc2/:loc3',
-  setLocationByNaverMapsApi,
+  userService.setLocationByNaverMapsApi,
 );
 
 //위치를 인자로 받아 동을 지워주는 api
-userRouter.delete('/location/:dong', verifyToken, deletelocation);
+userRouter.delete('/location/:dong', verifyToken, userService.deletelocation);
 
 //위치를 body로 받아 curLocation ABC를 채워주는 api
-userRouter.post('/location', verifyToken, addLocation);
+userRouter.post('/location', verifyToken, userService.addLocation);
 
 // router.delete('/:userId', async (req, res, next) => {
 //     try{
