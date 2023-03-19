3154c4cabd9919548f4eb7b2a26d5d2f17fc5579
kite707 <kite707707@gmail.com>
2023-03-19 18:43:01 +0900

    feat: util.ts에 success, fail 추가

--- src/modules/util.ts
+++ src/modules/util.ts
@@ -7,4 +7,19 @@ const jsonResponse = (res, code, message, isSuccess, result) => {
   });
 };
 
-export { jsonResponse };
+const success = (res, code, message, result) => {
+  res.status(code).json({
+    code: code,
+    message: message,
+    result: result,
+  });
+};
+
+const fail = (res, code, message) => {
+  res.status(code).json({
+    code: code,
+    message: message,
+  });
+};
+
+export { jsonResponse, success, fail };
