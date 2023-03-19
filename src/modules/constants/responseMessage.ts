db22c8ac59cc9f93a0291273e9705571d64cdd0d
kite707 <kite707707@gmail.com>
2023-03-19 20:29:35 +0900

    feat: 닉네임 변경 관련 responseMessage추가'

--- src/modules/constants/responseMessage.ts
+++ src/modules/constants/responseMessage.ts
@@ -5,6 +5,10 @@ const FORBIDDEN = '권한이 없습니다.';
 const INTERNAL_SERVER_ERROR = '서버에 에러가 발생했습니다.';
 const SERVICE_NOT_AVAILABLE = '서버를 사용할 수 없습니다.';
 
+const USER_NOT_FOUND = '해당되는 유저가 없습니다';
+const NICKNAME_DUPLICATED = '중복된 닉네임입니다.';
+const NICKNAME_CHANGE_SUCESS = '닉네임 변경을 완료하였습니다.';
+
 export {
   NOT_FOUND,
   BAD_REQUEST,
@@ -12,4 +16,7 @@ export {
   FORBIDDEN,
   INTERNAL_SERVER_ERROR,
   SERVICE_NOT_AVAILABLE,
+  USER_NOT_FOUND,
+  NICKNAME_DUPLICATED,
+  NICKNAME_CHANGE_SUCESS,
 };
