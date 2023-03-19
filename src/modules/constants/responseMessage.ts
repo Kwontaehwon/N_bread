a1865e78dfd99623b82eb019a81cbac91fff65e1
kite707 <kite707707@gmail.com>
2023-03-19 17:24:33 +0900

    feat: responseMessage 정의

---
+++ src/modules/constants/responseMessage.ts
@@ -0,0 +1,17 @@
+const NULL_VALUE = '필요한 값이 없습니다.';
+const OUT_OF_VALUE = '파라미터 값이 잘못되었습니다.';
+const NOT_FOUND = '잘못된 경로입니다.';
+const BAD_REQUEST = '잘못된 요청입니다.';
+const NO_USER = '존재하지 않는 유저';
+const NO_IMAGE = '이미지가 없습니다.';
+const UNAUTHORIZED = '권한이 없습니다.';
+
+export {
+  NULL_VALUE,
+  OUT_OF_VALUE,
+  NOT_FOUND,
+  BAD_REQUEST,
+  NO_USER,
+  NO_IMAGE,
+  UNAUTHORIZED,
+};
