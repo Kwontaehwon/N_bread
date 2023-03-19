23417de7e6a866706d0fa6283e37bbf7b0163d7e
kite707 <kite707707@gmail.com>
2023-03-19 20:29:11 +0900

    fix: util.ts success, fail함수 단순 json값 리턴하도록 변경

--- src/modules/util.ts
+++ src/modules/util.ts
@@ -7,19 +7,21 @@ const jsonResponse = (res, code, message, isSuccess, result) => {
   });
 };
 
-const success = (res, code, message, result) => {
-  res.status(code).json({
-    code: code,
-    message: message,
-    result: result,
-  });
+const success = (code: number, message: string, result?: any) => {
+  return {
+    code,
+    success: true,
+    message,
+    result,
+  };
 };
 
-const fail = (res, code, message) => {
-  res.status(code).json({
-    code: code,
-    message: message,
-  });
+const fail = (code: number, message: string) => {
+  return {
+    code,
+    success: false,
+    message,
+  };
 };
 
 export { jsonResponse, success, fail };
