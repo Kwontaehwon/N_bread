3ea8d696077c221e688fc5a97a945c06d7452897
kite707 <kite707707@gmail.com>
2023-03-19 20:30:09 +0900

    fix: errorHandler 타입지정

--- src/modules/error/errorHandler.ts
+++ src/modules/error/errorHandler.ts
@@ -1,7 +1,15 @@
 const { ErrorWithStatusCode } = require('./errorGenerator');
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ErrorWithStatusCode } from './errorGenerator';
+const express = require('express');
 const { fail } = require('../util');
-const errorHandler = (error, req, res, next): Response => {
+const errorHandler: ErrorRequestHandler = (
+  error: ErrorWithStatusCode,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   const { message, statusCode } = error;
-  return res.status(statusCode).send(fail(statusCode, message));
+  return res.status(statusCode).json(fail(statusCode, message));
 };
 export { errorHandler };
