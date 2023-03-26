019a7f0b00b4bb7f0d68a535b60ca860e380a822
권태현 <kygkth2011@gmail.com>
2023-03-27 03:31:12 +0900

    refactor : POST /deals JSON Reqestbody를 받는 dealParam 구현

---
+++ src/dto/deal/dealParam.ts
@@ -0,0 +1,14 @@
+type dealParam = {
+    link : String,
+    title : String,
+    content : String,
+    totalPrice : Number,
+    personalPrice : Number,
+    totalMember : Number,
+    dealDate : Date,
+    place : String,
+}   
+
+export {
+    dealParam
+}
