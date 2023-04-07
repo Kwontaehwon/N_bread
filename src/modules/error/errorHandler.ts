08fd2f0485b8b260f6ce329dc9199dc04246b574
권태현 <kygkth2011@gmail.com>
2023-04-08 02:38:25 +0900

    fix : 500 에러 발생시 res가 나가지 않던 오류 해결

--- src/modules/error/errorHandler.ts
+++ src/modules/error/errorHandler.ts
@@ -3,12 +3,14 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { ErrorWithStatusCode } from './errorGenerator';
 const express = require('express');
 const { fail } = require('../util');
+
 const errorHandler: ErrorRequestHandler = (
   error: ErrorWithStatusCode,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  if (error.statusCode == undefined) error.statusCode = 500; // customError를 발생시킨 것이 아닐 경우
   const { message, statusCode } = error;
   return res.status(statusCode).json(fail(statusCode, message));
 };
