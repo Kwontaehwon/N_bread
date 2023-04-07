c3eb17a65d794eed04e25ecc2054be72c499c084
권태현 <kygkth2011@gmail.com>
2023-04-08 02:39:54 +0900

    refactor : dealService.createDeal 구현

--- src/repository/dealRepository.ts
+++ src/repository/dealRepository.ts
@@ -1,27 +1,26 @@
-import { Deal } from "../database/models"
-import { errorGenerator } from "../modules/error/errorGenerator"
-import { responseMessage, statusCode } from "../modules/constants"
-import { dealParam } from "../dto/deal/dealParam";
-import { User } from "../database/models/user";
+import { Deal } from '../database/models';
+import { errorGenerator } from '../modules/error/errorGenerator';
+import { responseMessage, statusCode } from '../modules/constants';
+import { dealParam } from '../dto/deal/dealParam';
+import { User } from '../database/models/user';
 
-const createDeal = async (dealParam : dealParam, user:User) => {
-    return await Deal.create({
-        link: dealParam.link,
-        title: dealParam.title,
-        content: dealParam.content,
-        totalPrice: +dealParam.totalPrice,
-        personalPrice: +dealParam.personalPrice,
-        totalMember: +dealParam.totalMember,
-        dealDate: new Date(dealParam.dealDate), // 날짜 변환
-        dealPlace: dealParam.place,
-        currentMember: 1, // 내가 얼마나 가져갈지 선택지를 줘야할듯
-        userId: user.id,
-        loc1: user.curLocation1,
-        loc2: user.curLocation2,
-        loc3: user.curLocation3,
-      });
-}
+const createDeal = async (dealParam: dealParam, user: User) => {
+  const deal = await Deal.create({
+    link: dealParam.link,
+    title: dealParam.title,
+    content: dealParam.content,
+    totalPrice: +dealParam.totalPrice,
+    personalPrice: +dealParam.personalPrice,
+    totalMember: +dealParam.totalMember,
+    dealDate: new Date(dealParam.dealDate), // 날짜 변환
+    dealPlace: dealParam.place,
+    currentMember: 1, // 내가 얼마나 가져갈지 선택지를 줘야할듯
+    userId: user.id,
+    loc1: user.curLocation1,
+    loc2: user.curLocation2,
+    loc3: user.curLocation3,
+  });
+  return deal;
+};
 
-export {
-    createDeal
-}
+export { createDeal };
--- src/service/dealService.ts
+++ src/service/dealService.ts
@@ -1,51 +1,55 @@
-import axios from "axios";
-import { util } from "../modules/index";
-import {Deal, User, Group} from "../database/models"
-import { userRepository, groupRepository, dealRepository } from "../repository";
-import { dealParam } from "../dto/deal/dealParam";
-import { logger } from "../config/winston";
+import axios from 'axios';
+import { util } from '../modules/index';
+import { Deal, User, Group } from '../database/models';
+import { userRepository, groupRepository, dealRepository } from '../repository';
+import { dealParam } from '../dto/deal/dealParam';
+import { logger } from '../config/winston';
+import { errorGenerator } from '../modules/error/errorGenerator';
+import { responseMessage } from '../modules/constants';
+import { dealDto } from '../dto/deal/dealDto';
 
 const createDeal = async (req, res, next) => {
-    try {
-        const dealParam : dealParam = req.body; // currentMember 수정 필요.
-        const userId = req.decoded.id;
+  try {
+    const dealParam: dealParam = req.body; // currentMember 수정 필요.
+    const userId = req.decoded.id;
+    const user = await userRepository.findUserById(userId);
+    if (!user) {
+      throw errorGenerator({
+        message: responseMessage.NOT_FOUND,
+        code: 404,
+      });
+    }
+    const group = await groupRepository.createGroup(1, userId);
+    const deal = await dealRepository.createDeal(dealParam, user);
+    await group.update({ dealId: deal.id }); // 업데이트
+    const fcmTokenJson = await axios.get(
+      `https://d3wcvzzxce.execute-api.ap-northeast-2.amazonaws.com/tokens/${user.id}`,
+    );
+    if (Object.keys(fcmTokenJson.data).length !== 0) {
+      const fcmToken = fcmTokenJson.data.Item.fcmToken;
+      const fcmTopicName = `dealFcmTopic` + deal.id;
+      await admin.messaging().subscribeToTopic(fcmToken, fcmTopicName);
+    }
+    const dealDtos = new dealDto(
+      deal.id,
+      deal.link,
+      deal.title,
+      deal.content,
+      deal.totalPrice,
+      deal.personalPrice,
+      deal.totalMember,
+      deal.dealDate,
+      deal.dealPlace,
+      deal.loc1,
+      deal.loc2,
+      deal.loc3,
+    );
+    logger.info(`dealId : ${deal.id} 거래가 생성되었습니다.`);
+    return util.jsonResponse(res, 200, '거래가 생성되었습니다', true, dealDtos);
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
 
-        const user = await userRepository.findUserById(userId) || logger.info("INFONFON");
-        if (!user) {
-          logger.info(`userId : ${userId}에 매칭되는 유저가 없습니다.`);
-          return util.jsonResponse(
-            res,
-            404,
-            `userId : ${userId}에 매칭되는 유저가 없습니다.`,
-            false,
-            null,
-          );
-        }
-        const group = await groupRepository.createGroup(1, userId);
-        const deal = await dealRepository.createDeal(dealParam, user);
-        await group.update({ dealId: deal.id }); // 업데이트
-        const fcmTokenJson = await axios.get(
-          `https://d3wcvzzxce.execute-api.ap-northeast-2.amazonaws.com/tokens/${user.id}`,
-        );
-        if (Object.keys(fcmTokenJson.data).length !== 0) {
-          const fcmToken = fcmTokenJson.data.Item.fcmToken;
-          const fcmTopicName = `dealFcmTopic` + deal.id;
-          await admin.messaging().subscribeToTopic(fcmToken, fcmTopicName);
-        }
-        logger.info(`dealId : ${deal.id} 거래가 생성되었습니다.`);
-        return util.jsonResponse(res, 200, '거래가 생성되었습니다', true, deal);
-      } catch (error) {
-        logger.error(error);
-        return util.jsonResponse(
-          res,
-          500,
-          '[거래 생성] POST /deals/create 서버 에러',
-          false,
-          null,
-        );
-      }
-}
-
-export {
-    createDeal
-}
+export { createDeal };
