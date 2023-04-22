38dd3dfae90b56594817164e0bb81516e3504312
kite707 <12201680@inha.edu>
2023-04-22 21:46:40 +0900

    fix: userRepository import문 수정

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,4 +1,4 @@
-const userRepository = require('./userRepository');
-import * as groupRepository from './groupRepository'
-import * as dealRepository from './dealRepository'
-export { userRepository, groupRepository, dealRepository};
+import * as userRepository from './userRepository';
+import * as groupRepository from './groupRepository';
+import * as dealRepository from './dealRepository';
+export { userRepository, groupRepository, dealRepository };
