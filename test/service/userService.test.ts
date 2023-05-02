58078a3b78b3068b7c52349a0bc4e4d008ee742f
kite707 <12201680@inha.edu>
2023-05-02 10:22:38 +0900

    fix: 사용하지 않는 import 삭제

--- test/service/userService.test.ts
+++ test/service/userService.test.ts
@@ -2,7 +2,6 @@ import { getUser, changeUserNick } from '../../src/service/userService';
 import { User } from '../../src/database/models/user';
 import { responseMessage, statusCode } from '../../src/modules/constants';
 import * as util from '../../src/modules/util';
-import prisma from '../../src/prisma';
 import {
   errorGenerator,
   ErrorWithStatusCode,
