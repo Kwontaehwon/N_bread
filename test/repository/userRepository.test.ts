284139b5a29fb1b7a9091a39c7ae85f838ad8f65
kite707 <12201680@inha.edu>
2023-05-04 15:54:55 +0900

    feat: 닉네임 중복 여부 테스트코드 작성

--- test/repository/userRepository.test.ts
+++ test/repository/userRepository.test.ts
@@ -1,37 +1,37 @@
 import prisma from '../../src/prisma';
 import { PrismaClient } from '@prisma/client';
-import { findUserById } from '../../src/repository/userRepository';
+import {
+  findUserById,
+  isNicknameExist,
+} from '../../src/repository/userRepository';
+import { statusCode } from '../../src/modules/constants';
 const prismaForHardDelete = new PrismaClient();
 describe('find user by id', () => {
+  const user = {
+    email: 'testUser@gmail.com',
+    nick: '테스트유저',
+    password: 'test',
+    provider: 'local',
+    snsId: null,
+    accessToken: null,
+    curLocation1: 'test1',
+    curLocation2: 'test2',
+    curLocation3: 'test3',
+    curLocationA: null,
+    curLocationB: null,
+    curLocationC: null,
+    userStatus: null,
+    refreshToken: null,
+    isNewUser: true,
+    kakaoNumber: null,
+    createdAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
+    updatedAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
+    deletedAt: null,
+  };
   test('find user ', async () => {
-    const user = {
-      email: 'testUser@gmail.com',
-      nick: '테스트유저',
-      password: 'test',
-      provider: 'local',
-      snsId: null,
-      accessToken: null,
-      curLocation1: 'test1',
-      curLocation2: 'test2',
-      curLocation3: 'test3',
-      curLocationA: null,
-      curLocationB: null,
-      curLocationC: null,
-      userStatus: null,
-      refreshToken: null,
-      isNewUser: true,
-      kakaoNumber: null,
-      createdAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
-      updatedAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
-      deletedAt: null,
-    };
-    await prisma.users.create({ data: user });
-    const id = await prisma.users.findFirst({
-      where: { nick: '테스트유저' },
-      select: { id: true },
-    });
-    await expect(findUserById(+id.id)).resolves.toEqual({
-      id: id.id,
+    const createData = await prisma.users.create({ data: user });
+    await expect(findUserById(+createData.id)).resolves.toEqual({
+      id: createData.id,
       email: 'testUser@gmail.com',
       nick: '테스트유저',
       password: 'test',
@@ -53,14 +53,49 @@ describe('find user by id', () => {
       deletedAt: null,
     });
     await prismaForHardDelete.users.delete({
-      where: { id: id.id },
+      where: { id: createData.id },
     });
   });
   test('when user is not exist', async () => {
     try {
       await findUserById(1000);
     } catch (e) {
-      expect(e).toHaveProperty('statusCode', 400);
+      expect(e).toHaveProperty('statusCode', statusCode.NOT_FOUND);
     }
+  });
+});
+
+describe('isNicknameExist test', () => {
+  const user = {
+    email: 'testUser@gmail.com',
+    nick: '테스트유저',
+    password: 'test',
+    provider: 'local',
+    snsId: null,
+    accessToken: null,
+    curLocation1: 'test1',
+    curLocation2: 'test2',
+    curLocation3: 'test3',
+    curLocationA: null,
+    curLocationB: null,
+    curLocationC: null,
+    userStatus: null,
+    refreshToken: null,
+    isNewUser: true,
+    kakaoNumber: null,
+    createdAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
+    updatedAt: new Date(Date.parse('2022-05-04T12:47:04.000Z')),
+    deletedAt: null,
+  };
+  test('중복된 닉네임이 없을 때', async () => {
+    await expect(isNicknameExist('없는 닉네임')).resolves.toEqual(false);
+  });
+
+  test('중복된 닉네임일 경우', async () => {
+    const createData = await prisma.users.create({ data: user });
+    await expect(isNicknameExist('테스트유저')).resolves.toEqual(true);
+    await prismaForHardDelete.users.delete({
+      where: { id: createData.id },
+    });
   });
 });
