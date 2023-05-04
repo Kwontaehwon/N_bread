2532021442fd858b867dbb91b266c7f295bf244e
kite707 <12201680@inha.edu>
2023-05-04 14:20:36 +0900

    feat: prisma 모킹 파일 수정

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
 
-import prisma from './client';
+import prisma from './prisma';
 
-jest.mock('./client', () => ({
+jest.mock('./prisma', () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }));
@@ -12,7 +12,7 @@ beforeEach(() => {
   mockReset(prismaMock);
 });
 
-export const prismaMock = mockDeep<PrismaClient>() as unknown as DeepMockProxy<{
+export const prismaMock = prisma as unknown as DeepMockProxy<{
   // this is needed to resolve the issue with circular types definition
   // https://github.com/prisma/prisma/issues/10203
   [K in keyof PrismaClient]: Omit<PrismaClient[K], 'groupBy'>;
