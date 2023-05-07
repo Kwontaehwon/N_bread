ffe2bbb96cf5f06b5de09e2985b1c6c36f4b12a7
kite707 <12201680@inha.edu>
2023-05-07 17:56:16 +0900

    feat: authService에 localSignUp 구현

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { responseMessage, statusCode } from '../modules/constants';
-import { success } from '../modules/util';
+import { fail, success } from '../modules/util';
+import { userRepository } from '../repository';
+import bcrypt from 'bcrypt';
 const logout = async (req: Request, res: Response) => {
   req.logout(() => {
     req.session.destroy(() => {
@@ -11,4 +13,21 @@ const logout = async (req: Request, res: Response) => {
   });
 };
 
-export { logout };
+const localSignUp = async (req: Request, res: Response) => {
+  const { email, nick, password } = req.body;
+  const isEmailExist = await userRepository.isEmailExist(email);
+  if (isEmailExist) {
+    fail(res, statusCode.BAD_REQUEST, responseMessage.EMAIL_DUPLICATED);
+  }
+  const isNicknameExist = await userRepository.isNicknameExist(nick);
+  if (isNicknameExist) {
+    fail(res, statusCode.BAD_REQUEST, responseMessage.NICKNAME_DUPLICATED);
+  }
+
+  const hash = await bcrypt.hash(password, 12);
+  await userRepository.createUser(email, nick, hash);
+
+  success(res, statusCode.OK, responseMessage.SUCCESS);
+};
+
+export { logout, localSignUp };
