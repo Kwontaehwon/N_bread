3927e760c20950079eeacbf6892d9aa921fc21a1
권태현 <kygkth2011@gmail.com>
2023-05-14 20:44:00 +0900

    refactor : 거래 참여 service 리펙토링

--- src/service/dealService.ts
+++ src/service/dealService.ts
@@ -5,8 +5,9 @@ import { dealParam } from '../dto/deal/dealParam';
 import { logger } from '../config/winston';
 import { errorGenerator } from '../modules/error/errorGenerator';
 import { responseMessage, statusCode } from '../modules/constants';
-import { dealDto } from '../dto/deal/dealDto';
+import { DealDto } from '../dto/deal/dealDto';
 import prisma from '../prisma';
+import { GroupDto } from '../dto/groupDto';
 const admin = require('firebase-admin');
 
 const createDeal = async (req, res, next) => {
@@ -22,20 +23,7 @@ const createDeal = async (req, res, next) => {
       const fcmTopicName = `dealFcmTopic` + deal.id;
       await admin.messaging().subscribeToTopic(fcmToken, fcmTopicName);
     }
-    const dealDtos = new dealDto(
-      deal.id,
-      deal.link,
-      deal.title,
-      deal.content,
-      deal.totalPrice,
-      deal.personalPrice,
-      deal.totalMember,
-      deal.dealDate,
-      deal.dealPlace,
-      deal.loc1,
-      deal.loc2,
-      deal.loc3,
-    );
+    const dealDtos = new DealDto(deal);
     logger.info(`dealId : ${deal.id} 거래가 생성되었습니다.`);
     return success(res, statusCode.CREATED, responseMessage.SUCCESS, deal);
   } catch (error) {
@@ -79,4 +67,64 @@ const deleteDeal = async (req, res, next) => {
   }
 };
 
-export { createDeal, deleteDeal };
+const joinDeal = async (req, res, next) => {
+  const userId = req.params.userId;
+  const dealId = req.params.dealId;
+
+  const user = await userRepository.findUserById(userId);
+  const deal = await dealRepository.findDealById(dealId);
+  const isJoin = await groupRepository.findAlreadyJoin(userId, dealId);
+
+  if (isJoin) {
+    return fail(res, statusCode.FORBIDDEN, responseMessage.DEAL_ALREADY_JOINED);
+  }
+  if (deal.dealDate.getDate() < Date.now()) {
+    return fail(res, statusCode.FORBIDDEN, responseMessage.DEAL_DATE_EXPIRED);
+  }
+  const stock = deal.totalMember - deal.currentMember;
+  if (stock <= 0) {
+    return fail(
+      res,
+      statusCode.BAD_REQUEST,
+      responseMessage.DEAL_REQUEST_OUT_OF_STOCK,
+    );
+  }
+
+  const group = await groupRepository.createGroup(userId, dealId);
+
+  await prisma.deals.update({
+    data: { currentMember: deal.currentMember + 1 },
+    where: { id: deal.id },
+  });
+
+  const fcmTopicName = `dealFcmTopic` + deal.id;
+  // 그룹에 있는 모든 유저들에게
+  const message = {
+    notification: {
+      title: `N빵 신규 참여 알림`,
+      body: `${user.nick}님이 N빵에 참여하여 인원이 ${deal.currentMember} / ${deal.totalMember} 가 되었습니다!`,
+    },
+    data: {
+      type: 'deal',
+      dealId: `${deal.id}`,
+    },
+    topic: fcmTopicName,
+  };
+  await admin.messaging().send(message);
+
+  const fcmTokenJson = await axios.get(
+    `https://d3wcvzzxce.execute-api.ap-northeast-2.amazonaws.com/tokens/${user.id}`,
+  ); // ${user.id}
+  if (Object.keys(fcmTokenJson.data).length !== 0) {
+    const fcmToken = fcmTokenJson.data.Item.fcmToken;
+    logger.info(`fcmToken : ${fcmToken}`);
+    await admin.messaging().subscribeToTopic(fcmToken, fcmTopicName);
+  }
+  const groupDto = new GroupDto(group);
+  const dealDto = new DealDto(deal);
+
+  const returnJson = { groupDto, dealDto };
+  return success(res, statusCode.OK, responseMessage.SUCCESS, returnJson);
+};
+
+export { createDeal, deleteDeal, joinDeal };
