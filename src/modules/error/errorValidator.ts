048c1f1cfc0b484b9143bb39f035538a921907d2
kite707 <12201680@inha.edu>
2023-05-16 10:26:54 +0900

    fix: errorValidator fail함수 적용

--- src/modules/error/errorValidator.ts
+++ src/modules/error/errorValidator.ts
@@ -6,9 +6,7 @@ import { fail } from '../util';
 const errorValidator = (req: Request, res: Response, next: NextFunction) => {
   const errors: Result<ValidationError> = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(statusCode.BAD_REQUEST)
-      .send(fail(statusCode.BAD_REQUEST, responseMessage.BAD_REQUEST));
+    return fail(res, statusCode.BAD_REQUEST, responseMessage.BAD_REQUEST);
   }
   next();
 };
