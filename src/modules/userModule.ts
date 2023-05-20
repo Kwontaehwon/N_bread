fdb1265893c537dcbc1c42b47a1071b05b42e35a
kite707 <12201680@inha.edu>
2023-05-20 17:12:25 +0900

    feat: userModule에 _getLocationByCoordinate구현

--- src/modules/userModule.ts
+++ src/modules/userModule.ts
@@ -1,4 +1,8 @@
+import axios from 'axios';
 import { mypageDto } from '../dto/deal/mypageDto';
+import config from '../config';
+import { errorGenerator } from './error/errorGenerator';
+import { responseMessage, statusCode } from './constants';
 
 const _setDealStatus = (dataObject: mypageDto) => {
   let dDate = new Date(dataObject['dealDate']);
@@ -24,4 +28,30 @@ const _setUserStatus = (dataObject: mypageDto, suggestedDealId: number[]) => {
     dataObject['mystatus'] = '참여자';
   }
 };
-export { _setDealStatus, _setUserStatus };
+
+const _getLocationByCoordinate = (longitude: number, latitude: number) => {
+  const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${longitude},${latitude}&sourcecrs=epsg:4326&orders=legalcode&output=json`;
+
+  axios
+    .get(url, {
+      headers: {
+        'X-NCP-APIGW-API-KEY-ID': config.naverClientId!,
+        'X-NCP-APIGW-API-KEY': config.NaverClientSecret!,
+      },
+    })
+    .then(async (Response) => {
+      if (Response.data['status']['code'] === 200) {
+        throw errorGenerator({
+          code: statusCode.UNAUTHORIZED,
+          message: responseMessage.NAVER_UNAUTHORIZED,
+        });
+      } else if (Response.data['status']['code'] === 100) {
+        throw errorGenerator({
+          code: statusCode.BAD_REQUEST,
+          message: responseMessage.NAVER_INVALID_COORDINATE,
+        });
+      }
+      return Response.data['results'][0]['region'];
+    });
+};
+export { _setDealStatus, _setUserStatus, _getLocationByCoordinate };
