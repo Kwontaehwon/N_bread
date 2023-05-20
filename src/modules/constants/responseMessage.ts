81bc4227a208ca61492886633515a38a5b156267
kite707 <12201680@inha.edu>
2023-05-20 17:33:48 +0900

    feat: valid nickname responseMessage 추가

--- src/modules/constants/responseMessage.ts
+++ src/modules/constants/responseMessage.ts
@@ -6,24 +6,24 @@ export default {
   INTERNAL_SERVER_ERROR: '서버에 에러가 발생했습니다.',
   SERVICE_NOT_AVAILABLE: '서버를 사용할 수 없습니다.',
 
-  SUCCESS: '성공적으로 반환 하였습니다',
+  SUCCESS: '성공적으로 반환 하였습니다.',
+  VALID_NICKNAME: '사용할 수 있는 닉네임입니다.',
 
-  USER_NOT_FOUND: '해당되는 유저가 없습니다',
+  USER_NOT_FOUND: '해당되는 유저가 없습니다.',
   NICKNAME_DUPLICATED: '중복된 닉네임입니다.',
   EMAIL_DUPLICATED: '중복된 이메일입니다.',
   NICKNAME_CHANGE_FAIL: '닉네임 변경 중 오류가 발생하였습니다.',
   LOGIN_FAILED: '로그인에 실패하였습니다.',
-  CREATE_USER_FAILED: '유저 생성에 실패하였습니다',
-  SAVE_USER_LOCATION_FAILED: '유저 위치 저장에 실패하였습니다',
+  CREATE_USER_FAILED: '유저 생성에 실패하였습니다.',
+  SAVE_USER_LOCATION_FAILED: '유저 위치 저장에 실패하였습니다.',
 
   // deal
-  DEAL_NOT_FOUND: '해당되는 거래가 없습니다',
+  DEAL_NOT_FOUND: '해당되는 거래가 없습니다.',
   DEAL_DELETE_NOT_AUTHORIZED: '글을 삭제할 권한이 없습니다.',
   DEAL_ALREADY_PARTICIPATED: '참여자가 있으므로 거래를 삭제할 수 없습니다.',
 
   NAVER_UNAUTHORIZED: 'Naver ClientKey, Naver ClientSecretKey가 필요합니다.',
-  NAVER_INVALID_COORDINATE:
-    '좌표가 유효하지 않습니다. 올바른 좌표를 넣어주세요.',
+  NAVER_INVALID_COORDINATE: '좌표가 유효하지 않습니다.',
 
   TOKEN_EXPIRED: '만료된 토큰입니다.',
   TOKEN_INVALID: '잘못된 토큰입니다.',
