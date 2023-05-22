d070afe69bea4c96135dae354d29c22b1c2162b9
권태현 <kygkth2011@gmail.com>
2023-05-22 17:16:35 +0900

    refactor : ErrorValidator 오류 발생 위치 반환

--- src/modules/error/errorValidator.ts
+++ src/modules/error/errorValidator.ts
@@ -2,11 +2,25 @@ import { Request, Response, NextFunction } from 'express';
 import { Result, ValidationError, validationResult } from 'express-validator';
 import { statusCode, responseMessage } from '../constants';
 import { fail } from '../util';
+import { logger } from '../../config/winston';
 
 const errorValidator = (req: Request, res: Response, next: NextFunction) => {
   const errors: Result<ValidationError> = validationResult(req);
   if (!errors.isEmpty()) {
-    return fail(res, statusCode.BAD_REQUEST, responseMessage.ERROR_VALIDATOR_ERROR);
+    logger.info(JSON.stringify(errors));
+
+    const errorsArray = errors.array();
+    let paramsString = '';
+
+    errorsArray.forEach((error) => {
+      paramsString += error.param + ', ';
+    });
+
+    return fail(
+      res,
+      statusCode.BAD_REQUEST,
+      paramsString + responseMessage.ERROR_VALIDATOR_ERROR,
+    );
   }
   next();
 };
