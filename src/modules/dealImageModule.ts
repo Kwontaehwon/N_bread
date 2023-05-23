c236f75cacaaf8839b3cf75e1d2be1d27801671d
권태현 <kygkth2011@gmail.com>
2023-05-23 15:46:44 +0900

    style : dealImageModule 필요 없는 주석 삭제

--- src/modules/dealImageModule.ts
+++ src/modules/dealImageModule.ts
@@ -11,9 +11,6 @@ const _createDealImage = async (req, dealId: number) => {
   if (result.length > 0) {
     for (let url of result) {
       const tmpImage = await dealImageRepository.createDealImage(url, dealId);
-      //   logger.info(
-      //     `dealId : ${dealId}에 dealImageId : ${tmpImage.id} 가 생성되었습니다.`,
-      //   );
     }
   }
   return result;
