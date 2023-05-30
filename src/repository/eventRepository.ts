8571cec0f0f32b9d6640e6ccfc888c97c722b8da
kite707 <12201680@inha.edu>
2023-05-30 16:24:44 +0900

    feat: 모든 이벤트 조회 함수 구현

---
+++ src/repository/eventRepository.ts
@@ -0,0 +1,17 @@
+import { responseMessage, statusCode } from '../modules/constants';
+import { errorGenerator } from '../modules/error/errorGenerator';
+import prisma from '../prisma';
+
+const getAllEvents = async () => {
+  try {
+    const data = await prisma.events.findMany({});
+    return data;
+  } catch (error) {
+    throw errorGenerator({
+      code: statusCode.BAD_REQUEST,
+      message: responseMessage.BAD_REQUEST,
+    });
+  }
+};
+
+export { getAllEvents };
