be6e006491bfdc872c33a9e56d927111b5fc8229
kite707 <12201680@inha.edu>
2023-06-04 19:49:32 +0900

    feat:_searchProduct 구현

--- src/modules/priceModules/productModule.ts
+++ src/modules/priceModules/productModule.ts
@@ -1,7 +1,8 @@
 import { spawn } from 'child_process';
 import { errorGenerator } from '../error/errorGenerator';
 import { responseMessage, statusCode } from '../constants';
-
+import config from '../../config';
+import request from 'typescript-require';
 const _getProductName = async (title: string, gramToAdd: string) => {
   let answer = '';
   const result_01 = await spawn('python3', ['../modules/getTopic.py', title]);
@@ -18,4 +19,36 @@ const _getProductName = async (title: string, gramToAdd: string) => {
   });
   return productName;
 };
-export { _getProductName };
+
+const _searchProduct = async (productName: string) => {
+  var url =
+    'https://openapi.naver.com/v1/search/shop.json?query=' +
+    encodeURI(productName) +
+    '&display=4'; // JSON 결과
+  var options = {
+    url: url,
+    headers: {
+      'X-Naver-Client-Id': config.naverClientId,
+      'X-Naver-Client-Secret': config.NaverClientSecret,
+    },
+  };
+  let item: any[];
+  await request.get(options, async (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      item = JSON.parse(body)['items'];
+      if (item.length === 0)
+        throw errorGenerator({
+          code: statusCode.NOT_FOUND,
+          message: responseMessage.SEARCH_RESULT_NOT_FOUND,
+        });
+      return item;
+    } else {
+      throw errorGenerator({
+        code: statusCode.BAD_REQUEST,
+        message: responseMessage.SHOPPING_API_ERROR,
+      });
+    }
+  });
+  return item;
+};
+export { _getProductName, _searchProduct };
