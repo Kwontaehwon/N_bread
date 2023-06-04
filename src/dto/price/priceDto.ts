961fe20b2549b6e816b35b43d61a962081ad5640
kite707 <12201680@inha.edu>
2023-06-04 20:04:50 +0900

    feat: priceDetailDto 정의

--- src/dto/price/priceDto.ts
+++ src/dto/price/priceDto.ts
@@ -5,3 +5,16 @@ export interface priceDto {
   lPrice: number;
   mallName: string;
 }
+
+export interface priceDetailDto extends priceDto {
+  link: string;
+  hPrice: string;
+  productId: string;
+  productType: string;
+  maker?: string;
+  brand?: string;
+  category1?: string;
+  category2?: string;
+  category3?: string;
+  category4?: string;
+}
