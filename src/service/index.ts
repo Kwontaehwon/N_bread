aa09bf5b2c65ef8c55d7dce57894c1a07ecc3b4d
kite707 <12201680@inha.edu>
2023-06-04 17:10:03 +0900

    feat: priceService 인덱스에 추가

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,4 +2,5 @@ import * as userService from './userService';
 import * as dealService from './dealService';
 import * as authService from './authService';
 import * as eventService from './eventService';
-export { userService, dealService, authService, eventService };
+import * as priceService from './priceService';
+export { userService, dealService, authService, eventService, priceService };
