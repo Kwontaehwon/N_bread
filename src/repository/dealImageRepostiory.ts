1ed9a77aa27d2616a65141dbf737efc1fcf62ee2
kite707 <12201680@inha.edu>
2023-06-04 17:10:28 +0900

    feat: findDealImageById구현

--- src/repository/dealImageRepostiory.ts
+++ src/repository/dealImageRepostiory.ts
@@ -9,4 +9,11 @@ const createDealImage = async (url: string, dealId: number) => {
   });
 };
 
-export { createDealImage };
+/**dealId기반 거래 이미지 조회, 거래 이미지 없어도 에러 x */
+const findDealImageById = async (dealId: number) => {
+  return await prisma.dealImages.findFirst({
+    where: { dealId },
+  });
+};
+
+export { createDealImage, findDealImageById };
