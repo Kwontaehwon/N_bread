532286637ba262a197badd17e09a09caa7df9167
권태현 <kygkth2011@gmail.com>
2023-06-06 16:51:08 +0900

    fix : commentDto 에서 id 반환하도록 변경

--- src/dto/commentDto.ts
+++ src/dto/commentDto.ts
@@ -1,11 +1,13 @@
 import { comments } from '@prisma/client';
 
 class CommentDto {
+  id: number;
   userId: number;
   dealId: number;
   content: string;
 
   constructor(comment: comments) {
+    this.id = comment.id;
     this.userId = comment.userId;
     this.dealId = comment.dealId;
     this.content = comment.content;
