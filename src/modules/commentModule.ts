27ae7d77a1e6165022a3ec2f52896fec432c5e31
권태현 <kygkth2011@gmail.com>
2023-06-07 02:18:53 +0900

    refactor : 그룹 userId 추출, 그룹 DtoList 생성 모듈 구현

--- src/modules/commentModule.ts
+++ src/modules/commentModule.ts
@@ -1,21 +1,23 @@
-import { comments, replies } from '@prisma/client';
+import { comments, groups, replies } from '@prisma/client';
+import { CommentWithUserDto } from '../dto/commentDto';
+import { ReplyWithUserDto } from '../dto/replyDto';
+import { CommentWithReplyDto } from '../dto/commentWithReplyDto';
+import { GroupDto } from '../dto/groupDto';
 
-const markCommentStatus = async (
-  text: comments[] | replies[],
-  suggester,
-  groupMember,
-) => {
-  if (text['deletedAt'] != null) {
-    text['content'] = '삭제된 댓글입니다.';
+const extractGroupMemberId = async (groupList: groups[]) => {
+  let groupMember: number[] = [];
+  for (let i = 0; i < groupList.length; i++) {
+    groupMember.push(groupList[i]['userId']);
   }
-  // comments UserStatus
-  if (text['userId'] === suggester) {
-    text['User']['userStatus'] = '제안자';
-  } else if (groupMember.includes(text['userId'])) {
-    text['User']['userStatus'] = '참여자';
-  } else {
-    text['User']['userStatus'] = '';
-  }
+  return groupMember;
 };
 
-export { markCommentStatus };
+const makeGroupDtoList = async (groupList: groups[]) => {
+  let groupDtoList: GroupDto[] = [];
+  for (const group of groupList) {
+    groupDtoList.push(new GroupDto(group));
+  }
+  return groupDtoList;
+};
+
+export { extractGroupMemberId, makeGroupDtoList };
