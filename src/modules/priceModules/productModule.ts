8c08cb665074a59f1773049e9f8e645f7187e268
kite707 <12201680@inha.edu>
2023-06-06 14:26:17 +0900

    fix: request를 axios로 변경

--- src/modules/priceModules/productModule.ts
+++ src/modules/priceModules/productModule.ts
@@ -2,7 +2,7 @@ import { spawn } from 'child_process';
 import { errorGenerator } from '../error/errorGenerator';
 import { responseMessage, statusCode } from '../constants';
 import config from '../../config';
-import request from 'typescript-require';
+import axios from 'axios';
 const _getProductName = async (title: string, gramToAdd: string) => {
   let answer = '';
   const result_01 = await spawn('python3', ['../modules/getTopic.py', title]);
@@ -21,34 +21,36 @@ const _getProductName = async (title: string, gramToAdd: string) => {
 };
 
 const _searchProduct = async (productName: string) => {
-  const url =
-    'https://openapi.naver.com/v1/search/shop.json?query=' +
-    encodeURI(productName) +
-    '&display=4'; // JSON 결과
-  const options = {
-    url: url,
-    headers: {
-      'X-Naver-Client-Id': config.naverClientId,
-      'X-Naver-Client-Secret': config.NaverClientSecret,
-    },
-  };
-  let item: any[];
-  await request.get(options, async (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      item = JSON.parse(body)['items'];
-      if (item.length === 0)
+  try {
+    const url =
+      'https://openapi.naver.com/v1/search/shop.json?query=' +
+      encodeURI(productName) +
+      '&display=4'; // JSON 결과
+    const options = {
+      url: url,
+      headers: {
+        'X-Naver-Client-Id': config.naverClientId,
+        'X-Naver-Client-Secret': config.NaverClientSecret,
+      },
+    };
+
+    const response = await axios.get(url, options);
+    if (response.status == 200) {
+      const items = response.data['items'];
+      if (items.length === 0)
         throw errorGenerator({
           code: statusCode.NOT_FOUND,
           message: responseMessage.SEARCH_RESULT_NOT_FOUND,
         });
-      return item;
+      return items;
     } else {
       throw errorGenerator({
         code: statusCode.BAD_REQUEST,
         message: responseMessage.SHOPPING_API_ERROR,
       });
     }
-  });
-  return item;
+  } catch (error) {
+    throw error;
+  }
 };
 export { _getProductName, _searchProduct };
