0e5f5b4bb3e9185c1b3a1964e5f83899c3523c65
kite707 <12201680@inha.edu>
2023-06-06 20:26:00 +0900

    feat: authModule index에 추가

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -6,6 +6,7 @@ import * as dealModule from './dealModule';
 import * as dealImageModule from './dealImageModule';
 import * as priceModule from './priceModules/priceModule';
 import * as productModule from './priceModules/productModule';
+import * as authModule from './authModule';
 export {
   util,
   jwtHandler,
@@ -15,4 +16,5 @@ export {
   dealImageModule,
   priceModule,
   productModule,
+  authModule,
 };
