778b1793c8f9c9366853c86c5027b18e412aad4b
권태현 <kygkth2011@gmail.com>
2023-06-06 16:24:36 +0900

    refactor : sendMulticast 모듈 구현

--- src/modules/fcmHandler.ts
+++ src/modules/fcmHandler.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 import admin from 'firebase-admin';
+import {
+  Notification,
+  DataMessagePayload,
+} from 'firebase-admin/lib/messaging/messaging-api';
+import { MulticastMessage } from 'firebase-admin/lib/messaging/messaging-api';
+import { logger } from '../config/winston';
 
 const dealSubscribe = async (userId: number, dealId: number) => {
   const fcmTokenJson = await axios.get(
@@ -16,6 +22,26 @@ const sendToSub = async (topicMessage: TopicMessage) => {
   await admin.messaging().send(topicMessage);
 };
 
+const sendMulticast = async (
+  userId: number,
+  notification: Notification,
+  data: DataMessagePayload,
+) => {
+  logger.info(`거래 제안자 id : ${userId} 에게 새로운 댓글 알림을 보냅니다. `);
+  const fcmTokenJson = await axios.get(
+    `https://d3wcvzzxce.execute-api.ap-northeast-2.amazonaws.com/tokens/${userId}`,
+  );
+  if (Object.keys(fcmTokenJson.data).length !== 0) {
+    const fcmToken = fcmTokenJson.data.Item.fcmToken;
+    const multicastMessage: MulticastMessage = {
+      tokens: [fcmToken],
+      notification: notification,
+      data: data,
+    };
+    await admin.messaging().sendMulticast(multicastMessage);
+  }
+};
+
 const createNotifiation = async (title: string, body: string) => {
   return {
     title: title,
@@ -25,4 +51,4 @@ const createNotifiation = async (title: string, body: string) => {
 
 // const createData = async (type : string, )
 
-export { dealSubscribe, sendToSub, createNotifiation };
+export { dealSubscribe, sendToSub, createNotifiation, sendMulticast };
