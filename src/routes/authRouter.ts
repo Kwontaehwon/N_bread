81a8b3a641aaeddbc0824db7aa3210fb8bc9b72d
kite707 <12201680@inha.edu>
2023-06-06 22:38:52 +0900

    refactor: kakaoSignout 로직 분리

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -40,39 +40,6 @@ authRouter.post(
 
 authRouter.delete('/apple/signout', verifyToken, authService.appleSignOut);
 
-// authRouter.delete('/kakaosdk/signout', verifyToken, async (req, res, next) => {
-//   // #swagger.summary = '카카오 SDK 회원탈퇴'
-//   try {
-//     const user = await User.findOne({ where: { id: req.decoded.id } });
-//     const userId = req.decoded.id;
-//     console.log(user);
-//     if (!user) {
-//       logger.info(
-//         '[카카오 SDK 회원탈퇴] id에 해당되는 유저를 찾을 수 없습니다.',
-//       );
-//       return util.jsonResponse(
-//         res,
-//         404,
-//         '[카카오 SDK 회원탈퇴] id에 해당되는 유저를 찾을 수 없습니다.',
-//         false,
-//         null,
-//       );
-//     }
-//     await user.destroy();
-//     logger.info(`[카카오 회원 탈퇴] ${userId} 카카오 회원 탈퇴 완료`);
-//     return util.jsonResponse(res, 200, '카카오 탈퇴완료', true, null);
-//   } catch (error) {
-//     logger.error('[카카오 회원 탈퇴] /auth/kakaosdk/signout 서버 에러' + error);
-//     return util.jsonResponse(
-//       res,
-//       500,
-//       '[카카오 회원 탈퇴] /auth/kakaosdk/signout 서버 에러',
-//       false,
-//       null,
-//     );
-//   }
-// });
-
-//https://appleid.apple.com/auth/authorize?response_type=code&client_id=shop.chocobread.service&scope=email%20name&response_mode=form_post&redirect_uri=https://chocobread.shop/auth/apple/callback
+authRouter.delete('/kakaosdk/signout', verifyToken, authService.kakaoSignOut);
 
 export { authRouter };
