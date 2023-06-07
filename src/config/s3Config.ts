50d1ea9a64b6eaf9fe1aa4ad895a2492d0160fa9
권태현 <kygkth2011@gmail.com>
2023-06-07 18:55:56 +0900

    refactor : AWS SDK v3 적용

--- src/config/s3Config.ts
+++ src/config/s3Config.ts
@@ -1,11 +1,11 @@
-import AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
 import config from '../config';
-AWS.config.update({
+const awsConfiguration = {
   region: 'ap-northeast-2',
   accessKeyId: config.s3AccessKeyID,
   secretAccessKey: config.s3SecretAccessKey,
-});
+};
 
-const s3 = new AWS.S3();
+const s3 = new S3Client(awsConfiguration);
 
 export { s3 };
